a7bc77c118abf2d900da0432cd6c2bbc8b78962f
saunier <alexandresaunier.perso@gmail.com>
2024-01-26 17:55:42 +0100

    Initial commi

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/year', function(req, res, next) {
+  res.json({ year: new Date().getFullYear().toString() });
+});
+
+
+module.exports = router;
